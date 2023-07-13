317e6977de0b8dd5eb4a1d23669ece620ef48f17
yajatvishwakarma <104378532+yajatvishwakarma@users.noreply.github.com>
2023-07-13 10:42:19 +0000

    upload

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.post('/upload', function(req, res) {
+    console.log(req.files.foo);
+    if(req.files.foo) res.send("Got the File!")
+    else res.send("No File :/")
+  });
 app.listen(process.env.PORT || 3000)
